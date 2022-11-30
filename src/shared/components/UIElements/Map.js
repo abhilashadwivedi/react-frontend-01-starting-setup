978c9bcd811a211fb636d02cd1da21c9ca35f1bf
abhilashadwivedi <abhilashadwivedi74@gmail.com>
2022-11-30 18:34:15 +0530

    Managing state in the input component

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './Map.css';
+
+const Map = props =>{
+    return <div className="map-in">This is the Map because I don't want to use google maps js sdk </div>
+};
+
+export default Map;
